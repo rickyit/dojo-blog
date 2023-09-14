0303b2fca20af78f92ae01fc66a9c24e61cc84e0
Ricky Tibay <rickyit@hotmail.com>
2023-09-14 22:28:36 +0800

    finished training

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -20,6 +21,9 @@ const App = () => {
             </Route>
             <Route path="/blogs/:id"> 
               <BlogDetails />
+            </Route>
+            <Route path="*"> 
+              <NotFound />
             </Route>
           </Switch>
         </div>
--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,29 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+
+    setIsPending(true);
+    
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+      // history.go(-1);
+      history.push('/');
+    });
   }
 
   return (
@@ -37,7 +52,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add blog</button>
+        { !isPending && <button>Add blog</button>}
+        { isPending && <button disabled>Adding blog...</button>}
       </form>
     </div>
   )
